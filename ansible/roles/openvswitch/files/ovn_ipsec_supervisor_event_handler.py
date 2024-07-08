a584caf3ba9131de376cc7da6d08084e76ab5e2d
Filip Dobrovolny <dobrovolny.filip@gmail.com>
2024-07-08 11:17:45 +0200

    Rewrite event handler to xml
    This still does not work, however; for those coming after me, I'm pushing this, so you don't have to spend time on this version.

--- ansible/roles/openvswitch/files/ovn_ipsec_supervisor_event_handler.py
+++ ansible/roles/openvswitch/files/ovn_ipsec_supervisor_event_handler.py
@@ -1,28 +1,58 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 import os
 import sys
-from supervisor import childutils
+import xmlrpc.client
+
+
+def write_stdout(s):
+    # Only event listener protocol messages may be sent to stdout
+    sys.stdout.write(s)
+    sys.stdout.flush()
+
+
+def write_stderr(s):
+    sys.stderr.write(s)
+    sys.stderr.flush()
+
+
+def get_rpc_interface():
+    # Extract required environment variables
+    supervisor_server_url = "http://localhost:9001/RPC2"
+    username = os.getenv('SUPERVISOR_USERNAME', None)
+    password = os.getenv('SUPERVISOR_PASSWORD', None)
+
+    if username and password:
+        return xmlrpc.client.ServerProxy(f'http://{username}:{password}@{supervisor_server_url}')
+    else:
+        return xmlrpc.client.ServerProxy(supervisor_server_url)
+
 
 def main():
-    # Set up an XML-RPC interface to Supervisor
-    rpc = childutils.getRPCInterface(os.environ)
+    rpc = get_rpc_interface()
 
-    # Listen for events indefinitely
     while True:
-        headers, payload = childutils.listener.wait(sys.stdin, sys.stdout)
-        event_type = headers['eventname']
+        # Transition from ACKNOWLEDGED to READY
+        write_stdout('READY\n')
 
-        # Check if the event is a process state change related to exiting or stopping
-        if "PROCESS_STATE_STOPPED" in event_type or "PROCESS_STATE_EXITED" in event_type:
-            process_name = headers['processname']
+        # Read header line and print it to stderr
+        line = sys.stdin.readline()
+        write_stderr(line)
 
-            print(f"Process {process_name} has stopped or exited. Stopping all processes.")
+        # Read event payload and print it to stderr
+        headers = dict([x.split(':') for x in line.split()])
+        data = sys.stdin.read(int(headers['len']))
+        write_stderr(data)
+
+        event_type = headers.get('eventname')
+        if event_type in ("PROCESS_STATE_STOPPED", "PROCESS_STATE_EXITED"):
+            process_name = headers.get('processname')
+            write_stderr(f"Process {process_name} has stopped or exited. Stopping all processes.\n")
 
             # Stop all processes managed by Supervisor
             rpc.supervisor.stopAllProcesses()
 
-        # Acknowledge the event to Supervisor
-        childutils.listener.ok(sys.stdout)
+        # Transition from READY to ACKNOWLEDGED
+        write_stdout('RESULT 2\nOK\n')
 
 
 if __name__ == '__main__':
